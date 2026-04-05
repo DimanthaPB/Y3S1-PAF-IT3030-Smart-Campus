9cdb12aa2f3746651efe310c2c1d3eba0ce58cf7
Pasindu B. Dimantha <pasindubhagya2001@gmail.com>
2026-04-05 16:14:16 +0530

    Initial Commit with database and google oauth config

---
+++ backend/src/main/java/com/smartcampus/paf_project/PafProjectApplication.java
@@ -0,0 +1,13 @@
+package com.smartcampus.paf_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartcampus/paf_project/PafProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcampus.paf_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
